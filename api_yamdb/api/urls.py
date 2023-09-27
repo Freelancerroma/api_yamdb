3af1f58fd3d7bfa06803a7fa49f9013252a3d971
Владислав Тришкин <trishkin.vlad197@yandex.ru>
2023-09-27 16:02:36 +0300

    add urls

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import (CategoryViewSet, GenreViewSet,
+                    TitleViewSet)
+
+
+app_name = 'api'
+
+v1_router = routers.DefaultRouter()
+v1_router.register('category', CategoryViewSet, basename='category')
+v1_router.register('genre', GenreViewSet, basename='genre')
+v1_router.register('title', TitleViewSet, basename='title')
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
