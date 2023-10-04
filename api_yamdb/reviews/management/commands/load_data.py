35ca30621907620af1dbc08ebe2c423681550863
Владислав Тришкин <trishkin.vlad197@yandex.ru>
2023-10-04 23:08:25 +0300

    good

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -2,6 +2,7 @@ import csv
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.db.utils import IntegrityError
 
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
@@ -26,6 +27,22 @@ class Command(BaseCommand):
                 encoding='utf-8'
             ) as csv_file:
                 reader = csv.DictReader(csv_file)
-                model.objects.bulk_create(
-                    model(**data) for data in reader)
+                for data in reader:
+                    try:
+                        if model == Title:
+                            category_id = data.pop('category')
+                            category = Category.objects.get(pk=category_id)
+                            data['category'] = category
+                        if model == Review:
+                            author_id = data.pop('author')
+                            author = User.objects.get(pk=author_id)
+                            data['author'] = author
+                        if model == Comment:
+                            author_id = data.pop('author')
+                            author = User.objects.get(pk=author_id)
+                            data['author'] = author
+                            model.objects.create(**data)
+                    except IntegrityError:
+                        self.stdout.write(self.style.WARNING(
+                            'Запись уже существует'))
         self.stdout.write(self.style.SUCCESS('Данные загружены'))
